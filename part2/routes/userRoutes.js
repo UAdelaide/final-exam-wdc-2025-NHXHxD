9d773eddb28fadea91e0c2f106b5d8e74ddc2107
Hy Nguyen <hynguyen2004@gmail.com>
2025-06-20 10:31:13 +0000

    [automatic commit] 2025-06-20 10:31:13 UTC 120.20.239.5

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,11 +35,6 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// POST /login
-const express = require('express');
-const router  = express.Router();
-const db      = require('../models/db');
-
 // POST /api/users/login
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
