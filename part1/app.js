f8ac41b1fe097a143a9feb8aa50d40c42a2851f5
Hy Nguyen <hynguyen2004@gmail.com>
2025-06-20 05:10:31 +0000

    [automatic commit] 2025-06-20 05:10:30 UTC 120.20.198.48

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dogWalkRouter = require('./routes/dogWalk');
 var db = require('../db');
 
 var app = express();
