e7082e4c60d3ee6ce60bf8e4f9d5cb75c69bb234
Hy Nguyen <hynguyen2004@gmail.com>
2025-06-20 05:03:03 +0000

    [automatic commit] 2025-06-20 05:03:03 UTC 120.20.198.48

--- part1/routes/dogWalk.js
+++ part1/routes/dogWalk.js
@@ -3,13 +3,15 @@ var router = express.Router();
 var db = require('../db');
 
 router.get('/dogs', async(req, res) => {
-    const [rows] = await db.query(`
-        SELECT d.name AS dog_name,
-        d.size as size,
-        u.username AS owner_username
-        FROM Dogs d
-        JOIN Users u
-            ON d.owner_id = u.
+    try {
+        const [rows] = await db.query(`
+            SELECT d.name AS dog_name,
+            d.size as size,
+            u.username AS owner_username
+            FROM Dogs d
+            JOIN Users u
+                ON d.owner_id = u.user_id
+            `);
+    }
 
-        `)
 })
